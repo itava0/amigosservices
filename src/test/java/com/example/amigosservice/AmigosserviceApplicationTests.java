7de80f43025ad255e9168b9e83acf485c052bc3f
Ivan Tavarez <ivantavarez2@gmail.com>
2023-05-31 12:04:12 -0400

    initial setup

---
+++ src/test/java/com/example/amigosservice/AmigosserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amigosservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
